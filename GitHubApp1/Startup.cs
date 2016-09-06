3d13f09ee5138df10ac6e108a8c3e1b83c431ff1
kamils <kamils@KAMILSZ230>
2016-09-06 13:15:42 -0400

    Add project files.

---
+++ GitHubApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubApp1.Startup))]
+namespace GitHubApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
